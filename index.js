7a02ba95c8e35860537af9d33db268b2556ba176
Adam Kalamondo <kalmndo@gmail.com>
2023-05-26 15:36:26 +0700

    coba

--- index.js
+++ index.js
@@ -4,8 +4,35 @@ const FormData = require('form-data');
 const app = express();
 const port = 3000;
 
+const url = 'http://localhost:1500/api/';
+
+const COMMAND = {
+  START: 'start?mode=print&&',
+  EXIT_LOCKSCREEN: 'lockscreen/exit?',
+  SHOW_LOCKSCREEN: 'lockscreen/show?',
+};
+
 app.get('/', (req, res) => {
-  req.res.send('Hello World!');
+  // payment success
+  // exit lockscreen
+  // CTRL + TAB
+  // if session_end
+  // show lockscreen
+  // CTRL + TAB
+
+  if (req.query.event_type === 'payment_success') {
+    const uri = `${url}${COMMAND.EXIT_LOCKSCREEN}password="qK8BinizM8M9a3om"`;
+
+    // alt + tab dslrBooth
+  }
+
+  if (req.query.event_type === 'session_start') {
+    // alt tab chrome
+  }
+
+  if (req.query.event_type === 'session_end') {
+    // alt + tab to chrome
+  }
 });
 
 app.get('/start', async (req, res) => {
@@ -14,7 +41,7 @@ app.get('/start', async (req, res) => {
   const config = {
     method: 'get',
     maxBodyLength: Infinity,
-    url: 'http://localhost:1500/api/lockscreen/exit?password=""',
+    url: 'http://localhost:1500/api/lockscreen/exit?password="qK8BinizM8M9a3om"',
     headers: {
       ...data.getHeaders(),
     },
